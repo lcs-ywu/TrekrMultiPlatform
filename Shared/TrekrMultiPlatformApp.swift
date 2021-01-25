f794ae470baa073ae71fe63b8bc6a6f0194d0796
James Wu <ywu@lcs.on.ca>
2021-01-25 14:58:54 -0500

    Initial Commit

---
+++ Shared/TrekrMultiPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrMultiPlatformApp.swift
+//  Shared
+//
+//  Created by James Wu on 2021-01-25.
+//
+
+import SwiftUI
+
+@main
+struct TrekrMultiPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
